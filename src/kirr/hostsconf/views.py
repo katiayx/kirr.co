3991d2354c75f3ecb5c807790a65817ff8da9749
Katia Wu <katiayx@gmail.com>
2017-01-20 19:07:25 -0800

    added readme

--- src/kirr/hostsconf/views.py
+++ src/kirr/hostsconf/views.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_PATH = getattr(settings, "DEFAULT_REDIRECT_URL", 'www.kirr.co')
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", 'www.kirr.co')
 
 def wildcard_redirect(request, path=None):
 
-
 	new_url = DEFAULT_REDIRECT_URL
-    if path is not None:
-        new_url = DEFAULT_REDIRECT_URL + '/' + path
-    return HttpResponseRedirect(new_url)
+	if path is not None:
+		new_url = DEFAULT_REDIRECT_URL + '/' + path
+	return HttpResponseRedirect(new_url)
